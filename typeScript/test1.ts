fdeccb4847c25c39de7b56b6bda198a56eedc2bd
zcz <214426857@qq.com>
2023-08-18 23:44:26 +0800

    ts初始化

---
+++ typeScript/test1.ts
@@ -0,0 +1,24 @@
+let isDone: boolean = false
+
+let age: number = 123
+
+let firstName: string = 'myName'
+
+let myName: string = `111${firstName}`
+
+let u: undefined = undefined
+
+let n: null = null
+
+let num: number = undefined
+
+let str: string = null
+
+//undefined和null类型可以赋给任意其他类型
+
+let notSure: any = 4
+
+notSure = 'ASSS'
+
+//any类型可以任意赋值
+
