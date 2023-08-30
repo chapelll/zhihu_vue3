1ac41c1a7179c131140de9aa9677d3d2379bbe9b
zcz <214426857@qq.com>
2023-08-30 22:34:29 +0800

    初探vue3响应性

--- vue3-vite/src/App.vue
+++ vue3-vite/src/App.vue
@@ -1,32 +1,46 @@
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="world" />
+  <h1>{{ count }}</h1>
+  <h1>{{ double }}</h1>
+  <button @click="add">+1</button>
 </template>
 
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { ref, computed, reactive, toRefs } from 'vue';
+//ref变成响应式对象
+interface reactiveType {
+  count: number,
+  double: number,
+  add: () => void
+}
+
+
+export default {
+  name: 'App',
+  setup() {
+    const sum = ref(0)
+    // const count = ref(0)
+    // const double = computed(() => {
+    //   return count.value * 2
+    // })
+    // const add = () => {
+    //   count.value++
+    // }
+    const reactiveData: reactiveType = reactive({
+      count: 0,
+      add: () => {
+        return reactiveData.count++
+      },
+      double: computed(() => {
+        return reactiveData.count * 2
+      })
+    })
+    const reactiveRefData = toRefs(reactiveData)
+    return {
+      sum,
+      ...reactiveRefData
+    }
+  }
+}
 </script>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
