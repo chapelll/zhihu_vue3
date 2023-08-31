2727bb1dcfd8411cebfc6952774dcd8918c1643c
zhangcz <214426857@qq.com>
2023-08-31 17:25:01 +0800

    vue3的watch

--- vue3-vite/src/App.vue
+++ vue3-vite/src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <h1>{{ count }}</h1>
-  <h1>{{ double }}</h1>
+  <!-- <h1>{{ count }}</h1>
+  <h1>{{ sum }}</h1> -->
+  <div>{{ title }}</div>
   <button @click="add">+1</button>
+  <button @click="addHello" style="display: block;">+Hello</button>
 </template>
 
 <script lang="ts">
-import { ref, computed, reactive, toRefs, onMounted, onUpdated,onRenderTriggered, watch } from 'vue';
+import { ref, computed, reactive, toRefs, onMounted, onUpdated, onRenderTriggered, watch } from 'vue';
 //ref变成响应式对象
 interface reactiveType {
   count: number,
@@ -23,25 +25,50 @@ export default {
     onUpdated(() => {
       console.log('onUpdated');
     })
-    onRenderTriggered((event)=>{
+    onRenderTriggered((event) => {
       console.log(event);
     })
+
     const sum = ref(0)
+    const title = ref('')
     const reactiveData: reactiveType = reactive({
       count: 0,
       add: () => {
-        return reactiveData.count++
+        reactiveData.count++
+        sum.value++
+        return
       },
       double: computed(() => {
         return reactiveData.count * 2
       })
     })
+
+    const addHello = () => {
+      title.value = title.value + 'Hello'
+    }
+    const add = () => {
+      reactiveData.count++
+      sum.value++
+    }
+
+    watch([title, () => reactiveData.count], (newVal, oldVal) => {
+      console.log(newVal);
+      console.log(oldVal);
+      document.title = 'updated' + reactiveData.count + title.value
+    })
+
+
+
     const reactiveRefData = toRefs(reactiveData)
     return {
       sum,
-      ...reactiveRefData
+      ...reactiveRefData,
+      title,
+      add,
+      addHello
     }
-  }
+  },
+
 
 }
 </script>
