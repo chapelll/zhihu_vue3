6596d886d7330726a1b15d26ea7ababfd75daed3
zhangcz <214426857@qq.com>
2023-09-06 17:29:55 +0800

    点击外部关闭下拉菜单

--- zhifou/src/components/Dropdown.vue
+++ zhifou/src/components/Dropdown.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="dropdown">
+    <div class="dropdown" ref="dropdown">
       <a class="btn btn-outline-light my-2 dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
         data-bs-toggle="dropdown" aria-expanded="false" @click.prevent="toggleOpen">
         {{ title }}
@@ -13,7 +13,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, onMounted, onUnmounted } from 'vue'
 export default defineComponent({
   name: 'Dropdown',
   props: {
@@ -28,9 +28,28 @@ export default defineComponent({
       isOpen.value = !isOpen.value
     }
 
+    const dropdown = ref<null | HTMLElement>(null)
+    const handle = (e: MouseEvent) => {
+      //判断点击到的html是否在dropdown里
+      console.log(e.target);
+      
+      if (dropdown.value) {
+        if (!dropdown.value.contains(e.target as HTMLElement) && isOpen.value) {
+          isOpen.value = false
+        }
+      }
+    }
+    onMounted(() => {
+      document.addEventListener('click', handle)
+    })
+    onUnmounted(() => {
+      document.removeEventListener('click', handle)
+    })
     return {
       isOpen,
-      toggleOpen
+      toggleOpen,
+      dropdown,
+      handle
     }
   }
 })
