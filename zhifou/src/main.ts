0d742afa9c7b3c58666301d57ba39e68b0c8bdd6
zhangcz <214426857@qq.com>
2023-09-15 18:13:28 +0800

    添加vuex

--- zhifou/src/main.ts
+++ zhifou/src/main.ts
@@ -1,9 +1,25 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import { createStore } from 'vuex'
 import App from './App.vue'
 import Home from '../src/views/Home.vue'
 import Login from '../src/views/Login.vue'
 import ColumnDetail from '../src/views/ColumnDetail.vue'
+
+const store = createStore({
+    state: {
+        count: 0
+    },
+    mutations: {
+        add(state) {
+            state.count++
+        }
+    }
+})
+
+console.log(store.state.count);
+store.commit('add')
+console.log(store.state.count);
 
 const routerHistory = createWebHistory()
 const router = createRouter({
